71f0bfb0a27435ca9d811bd80a8d7295acc5c6de
boti2011 <bcamilo@gmail.com>
2021-01-14 20:58:38 -0500

    :construction: HU-1

---
+++ src/test/java/co/com/meli/challenge/register/alerts/AlertsApplicationTests.java
@@ -0,0 +1,11 @@
+package co.com.meli.challenge.register.alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
