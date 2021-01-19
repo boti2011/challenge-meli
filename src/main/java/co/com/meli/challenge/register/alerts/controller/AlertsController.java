46c102ba751c556449a3ace3c9c1b97ac1ffce83
boti2011 <bcamilo@gmail.com>
2021-01-18 22:37:17 -0500

    :memo: HT-6: Add swagger documentation

--- src/main/java/co/com/meli/challenge/register/alerts/controller/AlertsController.java
+++ src/main/java/co/com/meli/challenge/register/alerts/controller/AlertsController.java
@@ -3,25 +3,34 @@ package co.com.meli.challenge.register.alerts.controller;
 import co.com.meli.challenge.register.alerts.model.SaveAlertRequest;
 import co.com.meli.challenge.register.alerts.model.SaveAlertResponse;
 import co.com.meli.challenge.register.alerts.service.AlertService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/alerts")
 @RequiredArgsConstructor
+@Api(value = "API for register alerts")
 public class AlertsController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AlertsController.class);
 
   private final AlertService service;
 
+  @ApiOperation(value = "Create alerts with all properties", response = SaveAlertResponse.class)
+  @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Success alert create", response = SaveAlertResponse.class),
+        @ApiResponse(code = 500, message = "Technical Error without business control")
+      })
   @PostMapping("/create")
   public ResponseEntity<SaveAlertResponse> createAlert(@RequestBody SaveAlertRequest request) {
     LOGGER.info("ServerName: {}", request.getServerName());
--- src/main/java/co/com/meli/challenge/register/alerts/model/SaveAlertResponse.java
+++ src/main/java/co/com/meli/challenge/register/alerts/model/SaveAlertResponse.java
@@ -1,5 +1,7 @@
 package co.com.meli.challenge.register.alerts.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,8 +9,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@ApiModel(description = "Response structure to the alert creation")
 public class SaveAlertResponse {
 
+  @ApiModelProperty(notes = "Status response")
   private Integer status;
+
+  @ApiModelProperty(notes = "Description of response")
   private String description;
 }
